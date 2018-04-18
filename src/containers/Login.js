5342773f1d8018b44217f6de8eed0b02b9e6179b
hoanggia@gmail.com <hoanggia@gmail.com>
2018-04-18 10:22:55 -0400

    refactor to use mutation tag

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { graphql } from 'react-apollo';
-import { compose, withStateHandlers } from 'recompose';
+import { withStateHandlers } from 'recompose';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import { toast } from 'react-toastify';
+import { Mutation } from 'react-apollo';
 import {
   Container,
   Row,
@@ -28,7 +28,6 @@ const Login = ({
   handleForm,
   handleLogin,
   spinner,
-  login,
   setCredential,
   startSpinner,
   stopSpinner,
@@ -36,70 +35,74 @@ const Login = ({
 }) => {
   if (isLoggedIn) return <Redirect to="/dashboard" />;
   return (
-    <div className="app flex-row align-items-center">
-      <Container>
-        <Row className="justify-content-center">
-          <Col md="8">
-            <CardGroup>
-              <Card className="p-4">
-                <CardBody>
-                  <h1>Login</h1>
-                  <p className="text-muted">Sign In to your account</p>
-                  <InputGroup className="mb-3">
-                    <InputGroupAddon addonType="prepend">
-                      <InputGroupText>
-                        <i className="icon-user" />
-                      </InputGroupText>
-                    </InputGroupAddon>
-                    <Input
-                      type="text"
-                      placeholder="Username"
-                      value={form.username}
-                      onChange={event => handleForm('username', event.target.value)}
-                    />
-                  </InputGroup>
-                  <InputGroup className="mb-4">
-                    <InputGroupAddon addonType="prepend">
-                      <InputGroupText>
-                        <i className="icon-lock" />
-                      </InputGroupText>
-                    </InputGroupAddon>
-                    <Input
-                      type="password"
-                      placeholder="Password"
-                      value={form.password}
-                      onChange={event => handleForm('password', event.target.value)}
-                    />
-                  </InputGroup>
-                  <Row>
-                    <Col xs="6">
-                      {spinner ? (
-                        <Spinner />
-                      ) : (
-                        <Button
-                          color="primary"
-                          className="px-4"
-                          onClick={() =>
-                            handleLogin(login, setCredential, startSpinner, stopSpinner)
-                          }
-                        >
-                          Login
-                        </Button>
-                      )}
-                    </Col>
-                    <Col xs="6" className="text-right">
-                      <Button color="link" className="px-0">
-                        Forgot password?
-                      </Button>
-                    </Col>
-                  </Row>
-                </CardBody>
-              </Card>
-            </CardGroup>
-          </Col>
-        </Row>
-      </Container>
-    </div>
+    <Mutation mutation={LOGIN_MUTATION}>
+      {login => (
+        <div className="app flex-row align-items-center">
+          <Container>
+            <Row className="justify-content-center">
+              <Col md="8">
+                <CardGroup>
+                  <Card className="p-4">
+                    <CardBody>
+                      <h1>Login</h1>
+                      <p className="text-muted">Sign In to your account</p>
+                      <InputGroup className="mb-3">
+                        <InputGroupAddon addonType="prepend">
+                          <InputGroupText>
+                            <i className="icon-user" />
+                          </InputGroupText>
+                        </InputGroupAddon>
+                        <Input
+                          type="text"
+                          placeholder="Username"
+                          value={form.username}
+                          onChange={event => handleForm('username', event.target.value)}
+                        />
+                      </InputGroup>
+                      <InputGroup className="mb-4">
+                        <InputGroupAddon addonType="prepend">
+                          <InputGroupText>
+                            <i className="icon-lock" />
+                          </InputGroupText>
+                        </InputGroupAddon>
+                        <Input
+                          type="password"
+                          placeholder="Password"
+                          value={form.password}
+                          onChange={event => handleForm('password', event.target.value)}
+                        />
+                      </InputGroup>
+                      <Row>
+                        <Col xs="6">
+                          {spinner ? (
+                            <Spinner />
+                          ) : (
+                            <Button
+                              color="primary"
+                              className="px-4"
+                              onClick={() =>
+                                handleLogin(login, setCredential, startSpinner, stopSpinner)
+                              }
+                            >
+                              Login
+                            </Button>
+                          )}
+                        </Col>
+                        <Col xs="6" className="text-right">
+                          <Button color="link" className="px-0">
+                            Forgot password?
+                          </Button>
+                        </Col>
+                      </Row>
+                    </CardBody>
+                  </Card>
+                </CardGroup>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      )}
+    </Mutation>
   );
 };
 
@@ -108,7 +111,6 @@ Login.propTypes = {
   isLoggedIn: PropTypes.bool.isRequired,
   handleForm: PropTypes.func.isRequired,
   handleLogin: PropTypes.func.isRequired,
-  login: PropTypes.func.isRequired,
   spinner: PropTypes.bool.isRequired,
   setCredential: PropTypes.func.isRequired,
   startSpinner: PropTypes.func.isRequired,
@@ -124,40 +126,33 @@ const withRedux = connect(
   }
 )(Login);
 
-const enhance = compose(
-  graphql(LOGIN_MUTATION, {
-    props: ({ mutate }) => ({
-      login: variables => mutate({ variables })
-    })
+export default withStateHandlers(
+  ({ initial = { username: 'superadmin', password: '12345' } }) => ({
+    form: initial
   }),
-  withStateHandlers(
-    ({ initial = { username: 'superadmin', password: '12345' } }) => ({
-      form: initial
+  {
+    handleForm: ({ form }) => (key, value) => ({
+      form: { ...form, [key]: value }
     }),
-    {
-      handleForm: ({ form }) => (key, value) => ({
-        form: { ...form, [key]: value }
-      }),
-      handleLogin: ({ form }) => (login, setCredential, startSpinner, stopSpinner) => {
-        startSpinner();
-        login(form)
-          .then(({ data: { login: { token } } }) => {
-            //localStorage.setItem('token', token);
-            toast.success('Login success', {
-              position: toast.POSITION.BOTTOM_RIGHT
-            });
-            setCredential(token);
-            stopSpinner();
-          })
-          .catch(error => {
-            const { message } = error;
-            toast.error(message.split(':')[1], {
-              position: toast.POSITION.BOTTOM_RIGHT
-            });
-            stopSpinner();
+    handleLogin: ({ form }) => (login, setCredential, startSpinner, stopSpinner) => {
+      startSpinner();
+      login({ variables: form })
+        .then(({ data: { login: { token } } }) => {
+          // eslint-disable-next-line
+          localStorage.setItem('token', token);
+          toast.success('Login success', {
+            position: toast.POSITION.BOTTOM_RIGHT
           });
-      }
+          setCredential(token);
+          stopSpinner();
+        })
+        .catch(error => {
+          const { message } = error;
+          toast.error(message.split(':')[1], {
+            position: toast.POSITION.BOTTOM_RIGHT
+          });
+          stopSpinner();
+        });
     }
-  )
+  }
 )(withRedux);
-export default enhance;
